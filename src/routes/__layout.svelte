efd30e03cab96aeb03486fb7fb63eeb3084f1b83
Josef Bauer <josef.bauer.1st@gmail.com>
2022-02-11 17:00:48 +0100

    Ongoing design adaptions

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,14 +4,14 @@
   import "../tailwind.css";
 </script>
 
-<!--<div class="grid grid-rows-3 auto-rows-auto h-screen">-->
-<div class="sticky top-0 max-h-fit">
-  <Navbar />
+<div class="h-screen relative">
+  <div class="fixed top-0 max-h-fit w-screen">
+    <Navbar />
+  </div>
+  <div class="container mx-auto pt-20 pb-14 h-full">
+    <slot />
+  </div>
+  <div class="fixed bottom-0 max-h-fit w-screen">
+    <Footer />
+  </div>
 </div>
-<div class="container mx-auto">
-  <slot />
-</div>
-<div class="sticky bottom-0 max-h-fit">
-  <Footer />
-</div>
-<!--</div>-->
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,7 +10,7 @@
   let showTabIndex = 0;
 </script>
 
-<div class="flex flex-col gap-2">
+<div class="flex flex-col gap-2 h-full">
   <GeoLocation />
   <div class="tabs mt-6 max-h-8">
     <button on:click={() => (showTabIndex = 0)} class="tab tab-bordered grow" class:tab-active={showTabIndex === 0}>

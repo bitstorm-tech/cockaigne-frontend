0715db448664eb5c88c8b26c336cbb550b1f187e
Josef Bauer <josef.bauer.1st@gmail.com>
2022-04-29 23:28:16 +0200

    Improve deal item title

--- src/lib/components/dealer/DealsListItem.svelte
+++ src/lib/components/dealer/DealsListItem.svelte
@@ -8,10 +8,7 @@
 </script>
 
 <div class="flex gap-2 pr-2 items-center">
-  <div
-    class="flex flex-col bg-teal-500 grow rounded-r-xl text-gray-200 text-xl cursor-pointer"
-    class:h-14={!showDetails}
-  >
+  <div class="flex flex-col bg-teal-500 grow rounded-r-xl text-gray-200 cursor-pointer" class:h-12={!showDetails}>
     <div class="flex px-2 h-full items-center" on:click>{deal.title}</div>
     {#if showDetails}
       <DealListItemDetails {deal} {isUser} />

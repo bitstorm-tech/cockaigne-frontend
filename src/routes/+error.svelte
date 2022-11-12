1d7bbfe240baab0941a9a70f3379cbc008c81fe2
Josef Bauer <josef.bauer.1st@gmail.com>
2022-11-12 09:12:49 +0100

    Add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Button from "$lib/components/ui/Button.svelte";
+</script>
+
+<div class="flex flex-col p-8 gap-8 h-screen text-center">
+  <p>Ops, hier ist etwas schief gelaufen.</p>
+  <p>Anscheinend sind die ganzen kleinen Gizmos die in unseren Servern arbeiten gerade etwas verwirrt.</p>
+  <a href="/"><Button>Zurück auf Start</Button></a>
+</div>

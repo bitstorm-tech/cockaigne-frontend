2dad33b9416322652600567e0a902333f1b650f1
Josef Bauer <josef.bauer.1st@gmail.com>
2022-12-14 22:10:01 +0100

    Finally fix vercel dev

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
-/** @type {import('vite').UserConfig} */
-export default ({ mode }) => {
-  if (process.env.LOCAL_DEV) {
-    const env = loadEnv(mode, process.cwd());
-    process.env.DO_SPACES_REGION = env.VITE_DO_SPACES_REGION;
-    process.env.DO_SPACES_BASE_URL = env.VITE_DO_SPACES_BASE_URL;
-    process.env.DO_SPACES_BUCKET = env.VITE_DO_SPACES_BUCKET;
-    process.env.DO_SPACES_SECRET = env.VITE_DO_SPACES_SECRET;
-    process.env.DO_SPACES_KEY = env.VITE_DO_SPACES_KEY;
-    process.env.DO_SPACES_ENDPOINT = env.VITE_DO_SPACES_ENDPOINT;
-    process.env.PGCERT = env.VITE_PGCERT;
-    process.env.DATABASE_URL = env.VITE_DATABASE_URL;
-    process.env.JWT_SECRET = env.VITE_JWT_SECRET;
-  }
+/** @type {import("vite").UserConfig} */
+export default () => {
   return defineConfig({
     plugins: [sveltekit()],
     server: {

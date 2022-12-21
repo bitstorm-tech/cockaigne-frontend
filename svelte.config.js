d35b65d952e1c03e688611f1b9aa0cd7a11aab6f
Josef Bauer <josef.bauer.1st@gmail.com>
2022-12-21 13:59:50 +0100

    Enable edge functions

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
 import adapter from "@sveltejs/adapter-vercel";
 import preprocess from "svelte-preprocess";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter({ edge: true })
   }
 };
 

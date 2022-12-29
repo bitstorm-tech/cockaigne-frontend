04bca3d91bbbcc5dd026bff0beff3c0d130b6e6c
Josef Bauer <josef.bauer.1st@gmail.com>
2022-12-29 16:24:01 +0100

    Implement changelog

--- src/lib/components/nav/MobileMenu.svelte
+++ src/lib/components/nav/MobileMenu.svelte
@@ -5,6 +5,7 @@
   import { goto, invalidateAll } from "$app/navigation";
   import { page } from "$app/stores";
   import GearIcon from "$lib/components/ui/icons/GearIcon.svelte";
+  import NewsIcon from "$lib/components/ui/icons/NewsIcon.svelte";
 
   async function logout() {
     await fetch("/api/logout");
@@ -15,10 +16,23 @@
 
 <div class="flex flex-col gap-8 p-4 z-50 bg-base-300">
   {#if $page.data.user.isAuthenticated}
-    <a href="/settings" class="h-8 flex items-center gap-3"><GearIcon /> Einstellungen</a>
-    <button on:click={logout} class="h-8 flex items-center gap-3 cursor-pointer"><LogoutIcon /> Logout</button>
+    <a href="/settings" class="h-8 flex items-center gap-3">
+      <GearIcon />
+      Einstellungen
+    </a>
+    <a href="/changelog" class="h-8 flex items-center gap-3">
+      <NewsIcon />
+      Was gibt es Neues?
+    </a>
+    <button on:click={logout} class="h-8 flex items-center gap-3 cursor-pointer">
+      <LogoutIcon />
+      Logout
+    </button>
   {:else}
-    <a href="/" class="h-8 flex items-center gap-3"><LoginIcon /> Login</a>
+    <a href="/" class="h-8 flex items-center gap-3">
+      <LoginIcon />
+      Login
+    </a>
   {/if}
   <div class="mt-8">
     <LegalFooter />
---
+++ src/routes/changelog/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import changelog from "$lib/resources/changelog.json";
+
+  const dates = Object.keys(changelog);
+</script>
+
+<section class="p-5">
+  <h1 class="text-center">Was gibt es Neues?</h1>
+  {#each dates as date}
+    <h2 class="mt-5">{date}</h2>
+    <ul class="pl-4">
+      {#each changelog[date] as item}
+        <li class="list-disc">{item}</li>
+      {/each}
+    </ul>
+  {/each}
+</section>

6d42033dc0c92d74a7a17d450472184a4c59c455
Josef Bauer <josef.bauer.1st@gmail.com>
2023-05-12 17:49:41 +0200

    Fix bugs

--- src/lib/components/dealer/DealsList.svelte
+++ src/lib/components/dealer/DealsList.svelte
@@ -5,8 +5,7 @@
   import EmptyContent from "$lib/components/ui/EmptyContent.svelte";
   import GearIcon from "$lib/components/ui/icons/GearIcon.svelte";
   import Link from "$lib/components/ui/Link.svelte";
-  import type { Deal } from "$lib/database/deal/deal.model";
-  import type { ActiveDeal } from "$lib/supabase/public-types";
+  import type { ActiveDeal, Deal } from "$lib/supabase/public-types";
 
   export let deals: Deal[] | ActiveDeal[] = [];
   let openDetail = -1;
@@ -14,11 +13,11 @@
 
 <div class="flex flex-col gap-1">
   {#if deals.length === 0}
-    {#if $page.data.user.isDealer}
+    {#if $page.data.session.user.user_metadata.isDealer}
       <EmptyContent>
         <p>Du hast noch keine aktiven Deals. Worauf wartest du?</p>
         <p>
-          <Link href={"/deals/new?dealerId=" + $page.data.user.id} underline>Deal erstellen!</Link>
+          <Link href={"/deals/new?dealerId=" + $page.data.session.user.id} underline>Deal erstellen!</Link>
         </p>
       </EmptyContent>
     {:else}

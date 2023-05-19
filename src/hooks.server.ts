9dd7d3832e1e9332e85c72c7ca07d4a6a69b2992
Josef Bauer <josef.bauer.1st@gmail.com>
2023-05-19 08:38:02 +0200

    SSR issues

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,14 @@
-// src/hooks.server.ts
 import { PUBLIC_SUPABASE_API_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
 import { createSupabaseServerClient } from "@supabase/auth-helpers-sveltekit";
 import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-  event.locals.supabase = createSupabaseServerClient({
-    supabaseUrl: PUBLIC_SUPABASE_URL,
-    supabaseKey: PUBLIC_SUPABASE_API_KEY,
-    event
-  });
+	console.log("handle:", PUBLIC_SUPABASE_URL);
+	event.locals.supabase = createSupabaseServerClient({
+		supabaseUrl: PUBLIC_SUPABASE_URL,
+		supabaseKey: PUBLIC_SUPABASE_API_KEY,
+		event
+	});
 
   event.locals.getSession = async () => {
     const {

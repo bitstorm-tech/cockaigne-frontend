50f6539087ce3606835817c99526a8326a983d32
Josef Bauer <josef.bauer.1st@gmail.com>
2023-09-01 00:13:52 +0200

    Align new deal button with icons

--- src/routes/dealer/[id]/+page.svelte
+++ src/routes/dealer/[id]/+page.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-  import { page } from '$app/stores';
-  import DealsList from '$lib/components/dealer/DealsList.svelte';
-  import Pictures from '$lib/components/dealer/pictures/Pictures.svelte';
-  import RatingsList from '$lib/components/dealer/RatingsList.svelte';
-  import ProfileHeader from '$lib/components/profile/ProfileHeader.svelte';
-  import Button from '$lib/components/ui/Button.svelte';
-  import AddDealIcon from '$lib/components/ui/icons/AddDealIcon.svelte';
-  import DealIcon from '$lib/components/ui/icons/DealIcon.svelte';
-  import HeartIcon from '$lib/components/ui/icons/HeartIcon.svelte';
-  import LoadingSpinner from '$lib/components/ui/icons/LoadingSpinner.svelte';
-  import PhotoIcon from '$lib/components/ui/icons/PhotoIcon.svelte';
-  import RatingIcon from '$lib/components/ui/icons/RatingIcon.svelte';
-  import { toggleFavoriteDealer } from '$lib/supabase/dealer-service';
-  import { onMount } from 'svelte';
-  import type { PageData } from './$types';
+  import { page } from "$app/stores";
+  import DealsList from "$lib/components/dealer/DealsList.svelte";
+  import RatingsList from "$lib/components/dealer/RatingsList.svelte";
+  import Pictures from "$lib/components/dealer/pictures/Pictures.svelte";
+  import ProfileHeader from "$lib/components/profile/ProfileHeader.svelte";
+  import Button from "$lib/components/ui/Button.svelte";
+  import AddDealIcon from "$lib/components/ui/icons/AddDealIcon.svelte";
+  import DealIcon from "$lib/components/ui/icons/DealIcon.svelte";
+  import HeartIcon from "$lib/components/ui/icons/HeartIcon.svelte";
+  import LoadingSpinner from "$lib/components/ui/icons/LoadingSpinner.svelte";
+  import PhotoIcon from "$lib/components/ui/icons/PhotoIcon.svelte";
+  import RatingIcon from "$lib/components/ui/icons/RatingIcon.svelte";
+  import { toggleFavoriteDealer } from "$lib/supabase/dealer-service";
+  import { onMount } from "svelte";
+  import type { PageData } from "./$types";
 
   export let data: PageData;
   let activeTab = 0;
@@ -43,7 +43,7 @@
   category={account.category}
 >
   {#if $page.data.isDealer}
-    <a href={"/deals/new?dealerId=" + dealerId} class="mt-4">
+    <a href={"/deals/new?dealerId=" + dealerId}>
       <Button warning rightFlat additionalWidth><AddDealIcon size={4} /></Button>
     </a>
   {:else}
@@ -57,13 +57,13 @@
   {/if}
 </ProfileHeader>
 <div class="mb-1 mt-4 grid grid-cols-3">
-  <button class="tab-bordered tab" class:tab-active={activeTab === 0} on:click={() => (activeTab = 0)}>
+  <button class="tab tab-bordered" class:tab-active={activeTab === 0} on:click={() => (activeTab = 0)}>
     <DealIcon size={1.4} outline={activeTab !== 0} />
   </button>
-  <button class="tab-bordered tab" class:tab-active={activeTab === 1} on:click={() => (activeTab = 1)}>
+  <button class="tab tab-bordered" class:tab-active={activeTab === 1} on:click={() => (activeTab = 1)}>
     <PhotoIcon outline={activeTab !== 1} />
   </button>
-  <button class="tab-bordered tab" class:tab-active={activeTab === 2} on:click={() => (activeTab = 2)}>
+  <button class="tab tab-bordered" class:tab-active={activeTab === 2} on:click={() => (activeTab = 2)}>
     <RatingIcon size={1.4} outline={activeTab !== 2} />
   </button>
 </div>

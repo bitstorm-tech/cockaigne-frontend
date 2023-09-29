6f77cf2223dfd1c7c00b7064e155f3c51db7efe6
Josef Bauer <josef.bauer.1st@gmail.com>
2023-09-29 23:15:08 +0200

    Don't show dealer in favorite dealer page

--- src/lib/components/user/FavoriteDealersList.svelte
+++ src/lib/components/user/FavoriteDealersList.svelte
@@ -2,10 +2,9 @@
   import { page } from "$app/stores";
   import EmptyContent from "$lib/components/ui/EmptyContent.svelte";
   import { getActiveDealsByDealer } from "$lib/supabase/deal-service";
-  import { getFavoriteDealers, toggleFavoriteDealer } from "$lib/supabase/dealer-service";
+  import { getFavoriteDealers } from "$lib/supabase/dealer-service";
   import type { ActiveDeal, FavoriteDealer } from "$lib/supabase/public-types";
   import { onMount } from "svelte";
-  import HeartIcon from "../ui/icons/HeartIcon.svelte";
   import UserDealsList from "./UserDealsList.svelte";
 
   let dealers: FavoriteDealer[] = [];
@@ -19,13 +18,6 @@
     const dealerIds = dealers.map((dealer) => dealer.dealer_id!!);
     deals = await getActiveDealsByDealer(supabase, dealerIds);
   });
-
-  async function unfavorite(dealerId: string) {
-    const userId = $page.data.userId;
-    if (!userId) return;
-    await toggleFavoriteDealer($page.data.supabase, userId, dealerId);
-    dealers = dealers.filter((dealer) => dealer.dealer_id !== dealerId);
-  }
 </script>
 
 <div class="flex h-full flex-col justify-center gap-2 overflow-auto">
@@ -35,15 +27,9 @@
     </EmptyContent>
   {/if}
   {#each dealers as dealer}
-    <a href={`/dealer/${dealer.dealer_id}`} class="flex w-full justify-between bg-[#2c363a] p-3 text-[#b2b2b2]">
-      <p>{dealer.username}</p>
-      <button class="cursor-pointer" on:click|preventDefault={() => unfavorite(dealer.dealer_id || "")}>
-        <HeartIcon />
-      </button>
-    </a>
     <UserDealsList
       deals={deals.filter((deal) => deal.dealer_id === dealer.dealer_id)}
-      showCompanyName={false}
+      showCompanyName={true}
       showHotIcon={false}
       liveUpdate={false}
     />

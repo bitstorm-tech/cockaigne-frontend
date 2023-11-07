ca6050bab6427abe053be960164451d117fea469
Josef Bauer <josef.bauer.1st@gmail.com>
2023-11-07 23:57:46 +0100

    Add map to address settings

--- src/routes/settings/AddressSettings.svelte
+++ src/routes/settings/AddressSettings.svelte
@@ -1,10 +1,66 @@
 <script lang="ts">
+  import Button from "$lib/components/ui/Button.svelte";
   import Input from "$lib/components/ui/Input.svelte";
+  import { getLocation } from "$lib/geo/address.service";
+  import { toOpenLayersCoordinate } from "$lib/geo/geo.types";
   import type { Account } from "$lib/supabase/public-types";
+  import Feature from "ol/Feature";
+  import Map from "ol/Map";
+  import View from "ol/View";
+  import Point from "ol/geom/Point";
+  import TileLayer from "ol/layer/Tile";
+  import VectorLayer from "ol/layer/Vector";
+  import "ol/ol.css";
+  import { useGeographic } from "ol/proj";
+  import OSM from "ol/source/OSM";
+  import VectorSource from "ol/source/Vector";
+  import { Icon, Style } from "ol/style";
+  import { onMount } from "svelte";
 
   export let account: Account;
+  const pinLayerSource = new VectorSource();
+  const view = new View({
+    zoom: 16,
+    maxZoom: 20
+  });
+  const iconFeature = new Feature(new Point(account.location.coordinates));
+  pinLayerSource.addFeature(iconFeature);
 
-  let loading = false;
+  onMount(async () => {
+    useGeographic();
+
+    new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        }),
+        new VectorLayer({
+          source: pinLayerSource,
+          style: new Style({
+            image: new Icon({
+              scale: 0.1,
+              displacement: [0, 16],
+              src: "/icons/pin.png"
+            })
+          })
+        })
+      ],
+      view
+    });
+
+    view.setCenter(account.location.coordinates);
+  });
+
+  async function showOnMap() {
+    const coordinates = await getLocation(`${account.street} ${account.house_number}, ${account.zip} ${account.city}`);
+    if (coordinates) {
+      const olCoordinates = toOpenLayersCoordinate(coordinates);
+      view.setCenter(olCoordinates);
+      pinLayerSource.clear();
+      pinLayerSource.addFeature(new Feature(new Point(olCoordinates)));
+    }
+  }
 </script>
 
 <div class="grid grid-cols-3 gap-3">
@@ -19,3 +75,5 @@
   </div>
   <Input label="PLZ" bind:value={account.zip} />
 </div>
+<Button small on:click={showOnMap}>Auf Karte anzeigen</Button>
+<div id="map" class="h-52 w-full"></div>

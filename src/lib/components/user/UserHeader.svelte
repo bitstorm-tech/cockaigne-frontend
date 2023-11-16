06e94a649b20d0459f2b2bde52fc7d74dd15af79
Josef Bauer <josef.bauer.1st@gmail.com>
2023-11-16 14:29:06 +0100

    Same height for user and dealer header

--- src/lib/components/user/UserHeader.svelte
+++ src/lib/components/user/UserHeader.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import { page } from '$app/stores';
-  import ProfilePicture from '$lib/components/profile/ProfilePicture.svelte';
-  import LocationIcon from '$lib/components/ui/icons/LocationIcon.svelte';
-  import DealsBadge from '$lib/components/user/DealsBadge.svelte';
-  import FavoriteDealerBadge from '$lib/components/user/FavoriteDealerBadge.svelte';
-  import HotsBadge from '$lib/components/user/HotsBadge.svelte';
+  import { page } from "$app/stores";
+  import ProfilePicture from "$lib/components/profile/ProfilePicture.svelte";
+  import LocationIcon from "$lib/components/ui/icons/LocationIcon.svelte";
+  import DealsBadge from "$lib/components/user/DealsBadge.svelte";
+  import FavoriteDealerBadge from "$lib/components/user/FavoriteDealerBadge.svelte";
+  import HotsBadge from "$lib/components/user/HotsBadge.svelte";
 
   export let name = "";
   export let address: string[] = [""];
@@ -14,7 +14,7 @@
   export let deals = 0;
 </script>
 
-<div class="flex justify-between text-[#dbdce6]">
+<div class="flex h-48 justify-between text-[#dbdce6]">
   <div class="flex w-full justify-between">
     <div class="m-4 flex flex-col gap-4 pt-2">
       <div class="flex gap-2 fill-current text-[#69828c]" class:opacity-50={!$page.data.userId}>
